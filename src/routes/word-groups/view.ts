215621dc065b421b127e90e1c7761a5f038aeb18
JaiRam <praveengandhi.p@gmail.com>
2018-10-18 10:24:03 +0530

    viewVM base class added

--- src/routes/word-groups/view.ts
+++ src/routes/word-groups/view.ts
@@ -1,3 +1,4 @@
+import { BaseViewVM } from './../base/base-view';
 import { Subscription, EventAggregator } from 'aurelia-event-aggregator';
 import { WordsService } from './../../services/words-service';
 import { WordGroupsService } from './../../services/word-groups-service';
@@ -6,51 +7,55 @@ import { autoinject } from "aurelia-framework";
 import { Router } from "aurelia-router";
 
 @autoinject()
-export class View {
-  group: any;
-  words:any;
-  isLoading=false;
-  subscriber:Subscription;
-  constructor(private wordGroupsService: WordGroupsService, 
-    private wordsService:WordsService, private ea:EventAggregator, private router: Router, public appState:AppState) {}
+export class View extends BaseViewVM {
 
-  async activate(params: any) {
-    this.appState.loadingMessage = "Loading group details..!"
-    return this.wordGroupsService.get(params.id).then((d)=>{
-      this.group = d;
-      this.wordGroupsService.viewItem=this.group.name;
-      return this.group;
-    }).then((w)=>{
-      let query = {};
-      query[w.name]=1;
-      return this.wordsService.find({query}).then((d)=>{
-        this.words = d;
-        this.appState.loadingMessage='';
-        return this.words;
-      })
-    });
+  words: any;
+  isLoading = false;
+  subscriber: Subscription;
+  constructor(wordGroupsService: WordGroupsService,
+    private wordsService: WordsService, private ea: EventAggregator,
+    router: Router, public appState: AppState) {
+    super('group', wordGroupsService, router, appState);
   }
 
-  attached(){
-    this.subscriber = this.ea.subscribe('sortingChanged', data => {
-      this.isLoading=true;
+  async activate(params: any) {
+    this.promises = [this.mainService.get(params.id).then((d) => {
+      this.entity = d;
+      this.mainService.viewItem = this.entity.name;
+      return this.entity;
+    }).then((w) => {
       let query = {};
-      query[this.group.name]=1;
-      query['$sort']= data;
-      this.wordsService.find({query}).then((d)=>{
+      query[w.name] = 1;
+      return this.wordsService.find({
+        query
+      }).then((d) => {
         this.words = d;
-        this.isLoading=false;
+        this.appState.loadingMessage = '';
+        return this.words;
+      })
+    })];
+    this.promisesAdded = true;
+    return super.activate(params);
+  }
+
+  attached() {
+    this.subscriber = this.ea.subscribe('sortingChanged', data => {
+      this.isLoading = true;
+      let query = {};
+      query[this.entity.name] = 1;
+      query['$sort'] = data;
+      this.wordsService.find({
+        query
+      }).then((d) => {
+        this.words = d;
+        this.isLoading = false;
       });
     });
   }
-  detached(){
-    this.wordGroupsService.viewItem='';
+  
+  detached() {
+    this.mainService.viewItem = '';
+    this.subscriber.dispose();
   }
 
-  delete(){
-    if(!confirm('Are you sure?'))return;
-    this.wordGroupsService.delete(this.group._id).then((d)=>{
-      this.router.navigate('');
-    });
-  }
 }
--- src/routes/words/view.ts
+++ src/routes/words/view.ts
@@ -1,30 +1,14 @@
+import { BaseViewVM } from './../base/base-view';
 import { AppState } from '../../services/app-state';
 import { WordsService } from "../../services/words-service";
 import { autoinject } from "aurelia-framework";
 import { Router } from "aurelia-router";
 
 @autoinject()
-export class View {
-  word: any;
+export class View extends BaseViewVM{
   
-  constructor(private wordsService: WordsService, private router: Router, public appState:AppState) {}
-
-  async activate(params: any) {
-    this.appState.loadingMessage = "Loading word details..!"
-    return this.wordsService.get(params.id).then((d)=>{
-      this.word = d;
-      this.wordsService.viewItem=this.word.name;
-      this.appState.loadingMessage='';
-    });
+  constructor(wordsService: WordsService, router: Router, appState:AppState) {
+    super('word',wordsService,router,appState);
   }
-  detached(){
-    this.wordsService.viewItem='';
-  }
-
-  delete(){
-    if(!confirm('Are you sure?'))return;
-    this.wordsService.delete(this.word._id).then((d)=>{
-      this.router.navigate('');
-    });
-  }
+  
 }
